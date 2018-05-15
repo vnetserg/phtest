47af0271c97328e459adc4559c5aae4cf842cbc3
Sergei Fomin <sergio-dna@yandex.ru>
2018-05-15 11:03:25 +0300

    Русифицировал админку

--- phtest/admin.py
+++ phtest/admin.py
@@ -1,10 +1,42 @@
+from flask import request, session
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_babelex import Babel
+
+from phtest import app, db
+
 
 admin = Admin(app)
 
-class QuestionModelView(ModelView):
-    inline_models = (db.Answer,)
+class UserModelView(ModelView):
+    column_default_sort = "name"
+    column_labels = {
+        "name": "ФИО",
+        "login": "Логин",
+        "attempts": "Число попыток"
+    }
+    form_excluded_columns = ("right_questions",)
 
-admin.add_view(ModelView(db.User, db.session))
-admin.add_view(QuestionModelView(db.Question, db.session))
+class QuestionModelView(ModelView):
+    inline_models = [(db.Answer, {"column_labels": {
+        "text": "Текст",
+        "is_correct": "Верный"
+    }})]
+    column_labels = {
+        "text": "Текст",
+        "section_id": "Номер секции",
+        "answers": "Вариант Ответа"
+    }
+    column_default_sort = ("section_id", "text")
+
+admin.add_view(UserModelView(db.User, db.session, name="Пользователь"))
+admin.add_view(QuestionModelView(db.Question, db.session, name="Вопрос"))
+
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+    return session.get('lang', 'ru')
