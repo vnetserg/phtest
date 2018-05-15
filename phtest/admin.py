106f6a7d45028c85f81b958b1c8f828f6d62ea15
Sergei Fomin <sergio-dna@yandex.ru>
2018-05-15 11:25:06 +0300

    Добавил текст на главную админки

--- phtest/admin.py
+++ phtest/admin.py
@@ -1,12 +1,12 @@
-from flask import request, session
-from flask_admin import Admin
+from flask import request, session, url_for, redirect
+from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_babelex import Babel
 
 from phtest import app, db
 
 
-admin = Admin(app)
+admin = Admin(app, name="Тесторивание ФП")
 
 class UserModelView(ModelView):
     column_default_sort = "name"
@@ -29,8 +29,14 @@ class QuestionModelView(ModelView):
     }
     column_default_sort = ("section_id", "text")
 
-admin.add_view(UserModelView(db.User, db.session, name="Пользователь"))
-admin.add_view(QuestionModelView(db.Question, db.session, name="Вопрос"))
+class LogoutView(BaseView):
+    @expose("/")
+    def index(self):
+        return redirect(url_for("logout"))
+
+admin.add_view(UserModelView(db.User, db.session, name="Пользователи"))
+admin.add_view(QuestionModelView(db.Question, db.session, name="Вопросы"))
+admin.add_view(LogoutView(name="Выйти", endpoint="logout"))
 
 
 babel = Babel(app)
